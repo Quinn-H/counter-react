e99eece9d1995582c7b7d334ae05a1022d8dc9c0
Quinn-H <quinn.hu618@gmail.com>
2017-03-09 16:47:31 +1300

    add reducer function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+function reducer(state, action) {
+  if (action.type === 'INCREMENT') {
+    return state + action.amount;
+  } else if (action.type === 'DECREMENT') {
+    return state - action.amount;
+  } else {
+    return state;
   }
 }
 
-export default App;
+function createStore(reducer) {
+  let state = 0;
+
+  const getState = () => (state);
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+  };
+
+  return {
+    getState,
+    dispatch,
+  };
+}
+
+const store = createStore(reducer);
+
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 3,
+};
+
+store.dispatch(incrementAction);
+console.log(store.getState());
+store.dispatch(incrementAction);
+console.log(store.getState());
+
+const decrementAction = {
+  type: 'DECREMENT',
+  amount: 4,
+};
+
+store.dispatch(decrementAction);
+console.log(store.getState());
